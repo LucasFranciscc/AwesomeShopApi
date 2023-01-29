b8fd90b230acb37bb8b8e47f1cefb33b7542e64d
Keygen Solution <keygensolution@gmail.com>
2023-01-29 20:58:31 -0300

    Criação de api

---
+++ AwesomeShop.Services.Orders/AwesomeShop.Services.Orders.Application/Queries/GetOrderById.cs
@@ -0,0 +1,20 @@
+﻿using AwesomeShop.Services.Orders.Application.Dtos.ViewModels;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeShop.Services.Orders.Application.Queries
+{
+    public class GetOrderById : IRequest<OrderViewModel>
+    {
+        public GetOrderById(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
